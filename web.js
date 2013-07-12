74e437fbd98c4cfee38a10adc1bd8719405dbbc5
klwjack <klwjack@gmail.com>
2013-07-12 20:44:31 +0000

    what?

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  response.send("Hello");
 });
 
 var port = process.env.PORT || 5000;
